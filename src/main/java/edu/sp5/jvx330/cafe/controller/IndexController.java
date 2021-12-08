97002dfaf79e3fe18405d776713955df511ff7fa
Administrator <Administrator@DESKTOP-FU2AS1G>
2021-12-08 17:54:23 +0900

    20211208 - lcw
    Signed-off-by: Administrator <Administrator@DESKTOP-FU2AS1G>

--- src/main/java/edu/sp5/jvx330/cafe/controller/IndexController.java
+++ src/main/java/edu/sp5/jvx330/cafe/controller/IndexController.java
@@ -1,11 +1,11 @@
 package edu.sp5.jvx330.cafe.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import edu.sp5.jvx330.cafe.command.OrderItemsCommand;
 
 @Controller("main.index")
@@ -16,7 +16,7 @@ public class IndexController {
 	}
 	
 	@PostMapping("main/index")
-	public String index(@ModelAttribute OrderItemsCommand orderItems) {
+	public String index(@ModelAttribute List<OrderItemsCommand> orderItems_list) {
 		//거래내역 불러오기...? AJAX 안써도 될지도
 		return "main/index";
 	}
