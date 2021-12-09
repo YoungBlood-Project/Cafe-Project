3ac8746f981134c4d482cd9ba340bf6367162989
Administrator <Administrator@DESKTOP-FU2AS1G>
2021-12-09 15:20:48 +0900

    MileageHistory Rename - 정혜윤
    Signed-off-by: 정혜윤 <Administrator@DESKTOP-FU2AS1G>

--- src/main/java/edu/sp5/jvx330/cafe/customer/mileageHistory/dao/MileageHistoryDao.java
+++ src/main/java/edu/sp5/jvx330/cafe/customer/mileageHistory/dao/MileageHistoryDao.java
@@ -14,13 +14,13 @@ public interface MileageHistoryDao {
 	public void addMileageHistory(Long customerId, MileageHistory mileage);
 	
 	// 2. orderNum으로 마일리지 조회
-	public List<MileageHistory> findMileageHistoryByOrderNum(Long orderNum);
+	public List<MileageHistory> findMHByOrderNum(Long orderNum);
 	
 	/**
 	 * 초원
 	 */
 	// 1. 전체 마일리지 조회
-	public List<MileageHistory> findMileageByUserInfo(Customer customer);
+	public List<MileageHistory> findMHByUserInfo(Customer customer);
 	
 	// 2. 마일리지 금액 수정(마일리지 사용)
 	public void setMileageHistory(Customer customer, Integer mileage);
