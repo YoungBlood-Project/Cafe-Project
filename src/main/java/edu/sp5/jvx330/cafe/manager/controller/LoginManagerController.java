10d0510f7d4fed210c869c11f17d07d06d971c71
kch <kch1063@naver.com>
2021-12-13 16:46:15 +0900

    test annotaion 확인하려는데 run as 가 안뜸 - kch

--- src/main/java/edu/sp5/jvx330/cafe/manager/controller/LoginManagerController.java
+++ src/main/java/edu/sp5/jvx330/cafe/manager/controller/LoginManagerController.java
@@ -3,9 +3,11 @@ package edu.sp5.jvx330.cafe.manager.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import edu.sp5.jvx330.cafe.manager.business.impl.ManagerServiceImpl;
+import edu.sp5.jvx330.cafe.manager.domain.Manager;
 
 @Controller("manager.controller.LoginManagerController")
 public class LoginManagerController {
@@ -20,7 +22,7 @@ public class LoginManagerController {
 		
 		@PostMapping("manager/LoginManager")
 		public String managerLoginPost(@ModelAttribute Manager manager) {
-			managerService
+			return null;
 			
 			
 			
