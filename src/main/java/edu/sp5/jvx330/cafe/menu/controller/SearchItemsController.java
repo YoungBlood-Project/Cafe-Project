7e67174fb409e486c8005fd4fa73a6d21d363db2
user <user@DESKTOP-74ACKP5>
2021-12-15 18:18:39 +0900

    관리자로그인 화면전환 수정중 -kch

--- src/main/java/edu/sp5/jvx330/cafe/menu/controller/SearchItemsController.java
+++ src/main/java/edu/sp5/jvx330/cafe/menu/controller/SearchItemsController.java
@@ -4,12 +4,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import edu.sp5.jvx330.cafe.manager.domain.Manager;
 import edu.sp5.jvx330.cafe.menu.business.impl.CategoryServiceImpl;
 import edu.sp5.jvx330.cafe.menu.business.impl.ItemSerivceImpl;
 import edu.sp5.jvx330.cafe.menu.domain.Category;
@@ -24,13 +28,17 @@ public class SearchItemsController {
 	private CategoryServiceImpl categoryServiceImpl;
 	
 	@GetMapping("/menu/searchItems")
-	public ModelAndView SearchItemsGet(HttpSession session) {
-		
-
-		mav.addObject("requestURL", session.setRequestURL());
-		
-		
+	public ModelAndView SearchItemsGet(HttpServletRequest request, HttpSession session) {	
+		Manager auth_manager = (Manager)session.getAttribute("manager");
 		ModelAndView mav = new ModelAndView();
+		
+		//관리자로그인 확인
+		if(auth_manager == null) {
+			session.setAttribute("requestPath", request.getServletPath());
+			mav.setViewName("/manager/loginManager");
+			return mav;
+		}
+		
 		Map<Category, List<Item>> categoryItemMap = new HashMap<Category, List<Item>>();
 		List<Category> category_list = categoryServiceImpl.findAllCategorys();
 		for (Category category : category_list) {
