4175a30eee70ed5f1fd4e6a60b77e325aa5fbc0a
Administrator <Administrator@DESKTOP-87MI7RQ>
2021-12-16 09:28:10 +0900

    m_date_salesHistory 이름 수정 - jhy
    Signed-off-by: 정혜윤(develop-jhy) <Administrator@DESKTOP-87MI7RQ>

---
+++ src/main/java/edu/sp5/jvx330/cafe/sales/controller/MDateSalesHistoryController.java
@@ -0,0 +1,55 @@
+package edu.sp5.jvx330.cafe.sales.controller;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import edu.sp5.jvx330.cafe.menu.business.impl.CategoryServiceImpl;
+import edu.sp5.jvx330.cafe.menu.business.impl.ItemSerivceImpl;
+import edu.sp5.jvx330.cafe.menu.domain.Category;
+import edu.sp5.jvx330.cafe.menu.domain.Item;
+import edu.sp5.jvx330.cafe.sales.business.impl.SalesHistoryServiceImpl;
+import edu.sp5.jvx330.cafe.sales.business.impl.SalesTotalPriceServiceImpl;
+import edu.sp5.jvx330.cafe.sales.domain.SalesHistory;
+
+@Controller
+public class MDateSalesHistoryController {
+	
+	@Autowired
+	private CategoryServiceImpl categoryServiceImpl;
+	
+	@Autowired
+	private ItemSerivceImpl itemServiceImpl;
+	
+	@Autowired
+	private SalesHistoryServiceImpl salesHistoryServiceImpl;
+	
+	
+	@PostMapping("/sales/daySalesTotalPrice")
+	public ModelAndView daySalesTotalPost(String year, String month, String date) throws ParseException {
+		
+		ModelAndView mav = new ModelAndView();
+		
+		String dayString = year+"년 "+month+"월 "+date+"일";
+		
+		SimpleDateFormat sf = new SimpleDateFormat("yyyy년 MM월 dd일");
+		Date dateSales = sf.parse(dayString);
+	
+//		Map<Category, List<Item>> ciMap = new HashMap<Category, List<Item>>();
+		
+		List<SalesHistory> salesHistory_list = salesHistoryServiceImpl.findSalesHistoryByOrderDate(dateSales);
+		
+		
+		mav.addObject("salesHistory_list", salesHistory_list);
+		mav.setViewName("sales/day_salesTotalPrice");
+		return mav;
+	}
+}
