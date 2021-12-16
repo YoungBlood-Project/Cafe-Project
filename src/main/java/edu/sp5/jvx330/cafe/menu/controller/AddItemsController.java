bf1056e2ce14f64b48c74881d543cc1010354335
Administrator <Administrator@DESKTOP-87MI7RQ>
2021-12-16 11:34:46 +0900

    add_items 수정 중 - jhy
    Signed-off-by: 정혜윤(develop-jhy) <Administrator@DESKTOP-87MI7RQ>

--- src/main/java/edu/sp5/jvx330/cafe/menu/controller/AddItemsController.java
+++ src/main/java/edu/sp5/jvx330/cafe/menu/controller/AddItemsController.java
@@ -1,15 +1,44 @@
 package edu.sp5.jvx330.cafe.menu.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import edu.sp5.jvx330.cafe.menu.business.impl.CategoryServiceImpl;
+import edu.sp5.jvx330.cafe.menu.business.impl.ItemSerivceImpl;
+import edu.sp5.jvx330.cafe.menu.domain.Category;
+
 @Controller("addItemsContoller")
 public class AddItemsController {
-	@PostMapping("/menu/add_items")
-	public String addItemsPost() {
+	@Autowired
+	private CategoryServiceImpl categoryServiceImpl;
+	
+	@Autowired
+	private ItemSerivceImpl itemServiceImpl;
+	
+	@GetMapping("/menu/addItems")
+	public ModelAndView addItemGet() {
+		ModelAndView mav = new ModelAndView();
 		
-		return "/menu/search_items";
+		List<Category> category_list = categoryServiceImpl.findAllCategorys();
+
+		mav.addObject("category_list", category_list);
+		mav.setViewName("/main/addItems");
+		
+		return mav;
 	}
+	
+//	@PostMapping("/menu/addItems")
+//	public ModelAndView addItemsPost() {
+//		
+//		ModelAndView mav = new ModelAndView();
+//		
+//		
+//		
+//		
+//	}
 }
